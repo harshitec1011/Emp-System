8332e0468d56cac68ed1a5331a4c08d7b095ea0b
singh <harshit.c.singh@capgemini.com>
2023-08-29 16:35:22 +0530

    Initial commit

---
+++ src/main/java/com/Emp/System/Repository/EmpRepo.java
@@ -0,0 +1,9 @@
+package com.Emp.System.Repository;
+
+import com.Emp.System.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpRepo extends JpaRepository<Employee,Integer> {
+}
